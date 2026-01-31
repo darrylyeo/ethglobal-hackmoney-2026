2bbf5b77e15c60262672344e815b3ad35df63874
Darryl Yeo <dev@darryl-yeo.com>
2026-01-31 11:02:57 +0000

    Svelte: add useContext helper

---
+++ src/svelte/useContext.ts
@@ -0,0 +1,13 @@
+import { getContext, hasContext, setContext } from 'svelte'
+
+export const useContext = <
+	T
+>(
+	key: string,
+	getInitialValue: () => T
+) => (
+	hasContext(key) ?
+		getContext<T>(key)
+	:
+		setContext(key, getInitialValue())
+)
